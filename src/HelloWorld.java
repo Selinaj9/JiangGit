aa78cd89b47f96c388919e75b1888a2261cd5626
bths <sample@bths.edu>
2025-11-20 10:13:24 -0500

    change x's to y's and added print statements

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,15 +2,20 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello CSA");
         System.out.println("What's up");
-        int x = 5;
-        System.out.println("x is" + x);
-        x += 10;
-        x++;
-        System.out.println("x is now " + x);
-        if (x > 10) {
+        int y = 5;
+        System.out.println("x is" + y);
+        y += 10;
+        y++;
+        System.out.println("x is now " + y);
+        if (y > 10) {
             System.out.println("boop");
         } else {
             System.out.println("beep");
         }
+
+        System.out.println("fun with git");
+        System.out.println(y + 15);
+        System.out.println("yayaaaaayyyy");
+        System.out.println();
     }
 }

c2d4dbb53e44429ee8abd372e9169f5e289acd65
bths <sample@bths.edu>
2025-11-20 10:10:50 -0500

    added some logic

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello CSA");
+        System.out.println("What's up");
+        int x = 5;
+        System.out.println("x is" + x);
+        x += 10;
+        x++;
+        System.out.println("x is now " + x);
+        if (x > 10) {
+            System.out.println("boop");
+        } else {
+            System.out.println("beep");
+        }
     }
 }

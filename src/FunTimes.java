f4bca046f6b18c731873904ee1477c51154ee0ee
Selina <selinajiang25@gmail.com>
2025-11-20 18:11:30 -0500

    Commit from home

--- src/FunTimes.java
+++ src/FunTimes.java
@@ -2,5 +2,8 @@ public class FunTimes {
     public static void main(String[] args) {
         System.out.println("a new class!");
         System.out.println("cool");
+
+        System.out.println("Hi");
+        System.out.println("This is from Home!");
     }
 }
